01ce6ee4dc1c855412659a2cf71757a457372684
Lin Yiting <linytsysu@163.com>
2016-09-24 20:47:51 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,84 @@
 angular.module("apiCollector", [])
-    .controller("simpleCtrl", function($scope) {
-        $scope.viewFile = function () {
-            return "../html/content.html"
+    .controller("mainCtrl", function($scope) {
+        
+        $scope.view = "Content";
+        
+        $scope.data = {};
+        
+        $scope.shownTitle = "Used mostly";
+        $scope.shownData = {};
+        
+        $scope.getData = function() {
+            chrome.storage.sync.get("tags", function(data) {
+                $scope.$apply(function() {
+                    $scope.data.tags = data.tags;
+                });
+            });
+            
+            chrome.storage.sync.get("categories", function(data) {
+                $scope.$apply(function() {
+                    $scope.data.categories = data.categories;
+                });
+            });
+            
+            chrome.storage.sync.get("apis", function(data) {
+                $scope.data.apis = angular.toJson(data.apis);
+                $scope.$apply(function() {
+                    $scope.shownData = angular.toJson(data.apis);
+                });
+            });
         }
+        
+        $scope.getUsedMostly = function() {
+        }
+        
+        $scope.getDataByTag = function(tagName) {
+            targetData = [];
+            for(var i = 0; i < $scope.data.apis.length; i++) {
+                for (var j = 0; j < $scope.data.apis[i].tags.length; j++) {
+                    if ($scope.data.apis[i].tags[j] === tagName) {
+                        targetData.push($scope.data.apis[i]);
+                        break;
+                    }
+                }
+            }
+            $scope.shownData = targetData;
+        }
+        
+        $scope.getDataByCategory = function(categoryName) {
+            targetData = [];
+            for (var i = 0; i < $scope.data.apis.length; i++) {
+                if ($scope.data.apis[i].category === categoryName) {
+                    targetData.push($scope.data.apis[i]);
+                }
+            }
+            $scope.shownData = targetData;
+        }
+        
+        $scope.changeView = function(view) {
+            $scope.view = view;
+        }
+        
+        $scope.changeShownContent = function(mode, data) {
+            $scope.shownTitle = data; 
+            if (mode == 'category') {
+                $scope.getDataByCategory(data);
+            } else if (mode == 'tag') {
+                $scope.getDataByTag(data);
+            }
+        }
+        
+        $scope.addNewAPI = function(newAPI) {
+            var tags = newAPI.tags;
+            newAPI.tags = tags.split(',');
+            $scope.data.apis.push(newAPI);
+            // use angular.toJson instead of JSON.stringify
+            chrome.storage.sync.set({"apis": $scope.data.apis}, function() {
+                console.log("saved");
+            });
+            
+        }
+        
+        $scope.getData();
+        
     });
--- js/main.js
+++ js/main.js
@@ -52,17 +52,41 @@ function createFolder() {
     });
 }
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    createFolder();
-    
-    chrome.tabs.getSelected(null, function(tab) {
-        var tablink = tab.url;
-        console.log(tablink);
-        $('#show-test').text(tablink);
-        $('#show-test').attr('href', tablink);
+function testChromeStorage() {
+    chrome.storage.sync.set({"tags": ["jquery", "javascript", "python"]}, function() {
+        console.log("tags saved");
+    });
+    chrome.storage.sync.set({"categories": ["javascript", "python"]}, function() {
+        console.log("categories saved"); 
+    });
+    chrome.storage.sync.set({"apis": [
+        {"url": "http://jquery.com", "name": "jquery API", "category": "javascript", "tags": ["javascript", "jquery"]},
+        {"url": "https://docs.python.org/2/library/", "name": "python2.7 API", "category": "python", "tags": ["python"]}
+        ]}, function() {
+            console.log("apis saved");
     });
     
+    chrome.storage.sync.get("tags", function(data) {
+       console.log("tags: ");
+       console.log(data); 
+    });
+    chrome.storage.sync.get("categories", function(data) {
+       console.log("categories: ");
+       console.log(data); 
+    });
+    chrome.storage.sync.get("apis", function(data) {
+        console.log("apis: ");
+        console.log(data);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // testChromeStorage();
+    
+    chrome.tabs.getSelected(function(tab) {
+        console.log(tab);
+    });
+
     $('body').on('click', 'a#show-test', function(){
         chrome.tabs.create( {url: $(this).attr('href')} );
         return false;
@@ -72,6 +96,3 @@ document.addEventListener('DOMContentLoaded', function() {
     $('#toggle-helper').bind('click', hide_menu);
     
 });
-
-
-
